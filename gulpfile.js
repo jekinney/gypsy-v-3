d6d981b6ea0d3b00e61dc682273c9d2f368a9e98
Jason <jekinneys@gmail.com>
2016-01-02 21:14:46 -0600

    Added Bootstrap js

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,10 @@ elixir(function(mix) {
 	//mix.copy('node_modules/jquery/dist/jquery.js', 'resources/assets/js/jquery.js');
 	//mix.copy('node_modules/vue/dist/vue.js', 'resources/assets/js/vue.js');
 	//mix.copy('node_modules/vue-resource/dist/vue-resource.js', 'resources/assets/js/vue-resource.js');
+	//mix.copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', 'resources/assets/js/bootstrap.js');
 	//mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/frontend/fonts/bootstrap');
     mix.sass(['app.scss'], 'public/frontend/css/main.css');
-    mix.scripts(['jquery.js', 'vue.js', 'vue-resource.js'], 'public/frontend/js/main.js');
+    mix.scripts(['jquery.js', 'bootstrap.js', 'vue.js', 'vue-resource.js'], 'public/frontend/js/main.js');
 
     mix.version(['frontend/css/main.css', 'frontend/js/main.js']);
 });
